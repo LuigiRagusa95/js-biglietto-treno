32f904a218fa418560d81bc7be7e1b3bda7cb7ac
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-10-21 14:29:42 +0200

    first attempt

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const km = prompt('Quanti Km devi percorrere?');
+const age = prompt("Qual'è la tua eta?");
+200;
+let percentage = parseInt(km / parseFloat(0.21));
+let sale = null;
+
+if (age <= 18) {
+    sale = ((percentage / 100) * 20).toLocaleString('it', { minimumFractionDigits: 2 });
+} else if (age >= 65) {
+    sale = ((percentage / 100) * 40).toLocaleString('it', { minimumFractionDigits: 2 });
+} else {
+    sale = percentage.toLocaleString('it', { minimumFractionDigits: 2 });
+}
+
+document.querySelector('.info').innerHTML = `Ecco la tua tariffa ${sale}€`;
